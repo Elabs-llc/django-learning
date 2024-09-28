b62d8eda3ae56c18d07d86b728ac4d75bd903a8e
Elabs-llc <edwinasare08@gmail.com>
2024-09-28 03:05:33 -0700

    edit todo added

--- deepmind/todo_list/urls.py
+++ deepmind/todo_list/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('about', view=views.about, name='about'),
     path('delete/<list_id>', view=views.delete, name='delete'),
     path('complete/<list_id>', view=views.complete, name='complete'),
-    path('uncomplete/<list_id>', view=views.uncomplete, name='uncomplete')
+    path('uncomplete/<list_id>', view=views.uncomplete, name='uncomplete'),
+    path('edit/<list_id>', view=views.edit, name='edit')
+
 
 ]
--- deepmind/todo_list/views.py
+++ deepmind/todo_list/views.py
@@ -42,3 +42,18 @@ def uncomplete(request, list_id):
     messages.success(request, ('Todo item marked as uncompleted'))
     return redirect('home')
 
+
+def edit(request, list_id):
+    if request.method == 'POST':
+        item = List.objects.get(pk=list_id)
+        form = ListForm(request.POST or None, instance=item)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, ('Todo item updated successfully'))
+            return redirect('home')
+
+    else:
+        item = List.objects.get(pk=list_id)
+        return render(request, 'edit.html', {'item': item},)
+
